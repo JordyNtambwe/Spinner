4714f466efe7b87da44ad5e528645e9bbd47733c
JordyNtambwe <jordyntambwe@gmail.com>
2024-05-13 21:24:51 -0600

    Refactor spinner1!

---
+++ spinner2.js
@@ -0,0 +1,11 @@
+const spinner = ['\r|   ', '\r/   ', '\r-   ', '\r\\   ', '\r|   ', '\r/   ', '\r-   ', '\r\\   ', '\r|   \n'];
+
+let delay = 100;
+
+for (const char of spinner) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, delay);
+  delay += 200;
+
+};
